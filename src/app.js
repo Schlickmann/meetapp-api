c8c5cf8e683d52b65ef70372cfc87e011d47950d
Juliani Schlickmann Damasceno <nany.julychi@gmail.com>
2019-08-18 11:36:07 -0700

    Started MeetApp API

---
+++ src/app.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import cors from 'cors';
+
+import routes from './routes';
+
+import './database';
+
+require('dotenv').config();
+
+class App {
+  constructor() {
+    this.server = express();
+
+    this.middleware();
+    this.routes();
+  }
+
+  middleware() {
+    this.server.use(cors());
+    this.server.use(express.json());
+  }
+
+  routes() {
+    this.server.use(routes);
+  }
+}
+
+export default new App().server;
---
+++ src/config/auth.js
@@ -0,0 +1,6 @@
+require('dotenv').config();
+
+export default {
+  secret: process.env.JWT_SECRET,
+  ttl: process.env.JWT_TTL,
+};
